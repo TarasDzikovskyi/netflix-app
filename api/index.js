455433a6cf13e1b84ce85649e07e4583393db0a5
TarasDzikovskyi <tarasdz12367@gmail.com>
2023-02-15 20:38:21 +0200

    test commit

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ const expressFileUpload = require('express-fileupload');
 
 dotenv.config();
 const {authRouter, userRouter, movieRouter, listRouter} = require('./routes');
+const path = require("path");
 
 async function db_connect() {
     try {
@@ -25,13 +26,18 @@ db_connect();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressFileUpload());
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/movies", movieRouter);
 app.use("/api/lists", listRouter);
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
 
 
 app.listen(process.env.PORT, () => {
